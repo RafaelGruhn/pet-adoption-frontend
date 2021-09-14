c495a620cb3ba1fee393587eab3a8693426795db
rafael.gruhn <rafa.gruhn@gmail.com>
2021-09-14 19:29:45 -0300

    Add user image

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,11 +6,16 @@ import { getUserData } from "../service/user";
 
 export default function NavBar() {
     const token = getToken()
-    let user;
-    let json = "";
+    let user
+    let json = ""
+    let image = "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"
     if (token !== null) {
         json = getUserData(token)!!
         user = JSON.parse(json)
+        console.log(user)
+        if (user.photos.length > 0){
+            image = user.photos[0].url? user.photos[0].url!! : image
+        }
     }
     return (
         <Navbar expand="lg" bg="dark" variant="dark" className="fixed-top mb-5">
@@ -25,7 +30,10 @@ export default function NavBar() {
                         <Link to="/registerPet" className="underlineHover mr-4">Cadastrar Pet</Link>
                     </Navbar.Text>
                     <Navbar.Text>
-                        <Link to="/" className="underlineHover">Sair</Link>
+                        <Link to="/" className="underlineHover mr-4">Sair</Link>
+                    </Navbar.Text>
+                    <Navbar.Text>
+                        <img src={ image } className="mr-4" height="50" width="50" />
                     </Navbar.Text>
                 </Navbar.Collapse>
             </Container>
