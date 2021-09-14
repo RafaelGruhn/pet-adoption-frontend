e3da77050e9cddaf88942664bc33ad75fd59c5b2
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-09-14 15:21:30 -0300

    feat: carregando cards com os dados do banco

--- src/pages/Match/index.tsx
+++ src/pages/Match/index.tsx
@@ -7,34 +7,43 @@ import Footer from '../../components/footer';
 import NavBar from '../../components/navbar';
 import api from '../../service/api';
 
+interface IState {
+  data: Array<Object>;
+}
 
-export default function Match() {
-  let cards: JSX.Element[] = []
+export default class Match extends React.Component{
 
-  async function handleGetPets(e: { preventDefault: () => void; }) {
-    e.preventDefault();
-
-    const response = await api.get('pet?accountStatus=active')
-    console.log(response)
-    const data = response.data
-    for (let i = 0; i < data.length; i++){
-      console.log(data[i])
-      let pet_image = ""
-      if (data[i].photos.length > 0){
-        pet_image = data[i].photos[0].url? data[i].photos[0].url!! : ''
-      }
-      const pet_name = data[i].name? data[i].name!! : 'Dog'
-      // const pet_specie = data[i].specie? data[i].specie!! : 'Vira-Lata'
-      // const pet_breed = data[i].breed? data[i].breed!! : 'S/I'
-      const pet_size = data[i].size? data[i].size!! : 'S/I'
-      const estimatedAge = data[i].estimatedAge? data[i].estimatedAge!! : 'S/I'
-      const pet_description = data[i].description? data[i].description!! : ''
-      cards.push(<ImgMediaCard image={ pet_image } name={ pet_name } description={ pet_description } height={ pet_size } estimated_age={ estimatedAge }/>)
-
-    }
+  state = {
+    data: null
   }
 
-  return (
+  componentDidMount() {
+    api.get('pet?accountStatus=active')
+      .then((response) => {
+        let cards = []
+        console.log(response)
+        const data = response.data
+        for (let i = 0; i < data.length; i++){
+          console.log(data[i])
+          let pet_image = ""
+          if (data[i].photos.length > 0){
+            pet_image = data[i].photos[0].url? data[i].photos[0].url!! : ''
+          }
+          const pet_name = data[i].name? data[i].name!! : 'Dog'
+          // const pet_specie = data[i].specie? data[i].specie!! : 'Vira-Lata'
+          // const pet_breed = data[i].breed? data[i].breed!! : 'S/I'
+          const pet_size = data[i].size? data[i].size!! : 'S/I'
+          const estimatedAge = data[i].estimatedAge? data[i].estimatedAge!! : 'S/I'
+          const pet_description = data[i].description? data[i].description!! : ''
+          cards.push(<ImgMediaCard image={ pet_image } name={ pet_name } description={ pet_description } height={ pet_size } estimated_age={ estimatedAge }/>)
+        }
+        this.setState({ data: cards})
+      })
+  }
+
+  render () {
+
+    return (
       <div className="content">
         <Helmet>
           <link rel="stylesheet" href="css/bootstrap.min.js" />
@@ -42,10 +51,14 @@ export default function Match() {
         </Helmet>
         <NavBar/>
         <div className="body">
-          <Row onLoad={ handleGetPets }>
+          <Row>
             <div className="col-md-2"></div>
             <div className="col-md-8">
-              { cards }
+              {
+                this.state.data
+                ? this.state.data
+                : 'carregando'
+              }
             </div>
             <div className="col-md-2"></div>
           </Row>
@@ -53,4 +66,6 @@ export default function Match() {
         <Footer/>
       </div>
     );
+  }
+
 }
